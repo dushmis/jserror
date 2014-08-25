6550dbc899b4fb585e2d16eaac356e0f38483b57
dushyant <dushmis@gmail.com>
2014-08-25 16:59:10 +0530

    admin+test

--- main.py
+++ main.py
@@ -8,20 +8,21 @@ from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
 
 from google.appengine.ext import ndb
 
+from google.appengine.ext import db
+
 from google.appengine.ext import blobstore
 from google.appengine.api import files
 
 from google.appengine.api import users
 
 
-
-class jserror(ndb.Model):
-  err_ = ndb.StringProperty()
-  content_ = ndb.TextProperty()
-  page_ = ndb.StringProperty()
-  domain_ = ndb.StringProperty()
-  ip_ = ndb.StringProperty()
-  date = ndb.DateTimeProperty(auto_now_add=True)
+class jserror(db.Model):
+  err_ = db.StringProperty()
+  content_ = db.TextProperty()
+  page_ = db.StringProperty()
+  domain_ = db.StringProperty()
+  ip_ = db.StringProperty()
+  date = db.DateTimeProperty(auto_now_add=True)
 
 class MainHandler(webapp2.RequestHandler):
   def get(self):
@@ -35,7 +36,7 @@ class MainHandler(webapp2.RequestHandler):
       ddr=jserror(err_=b["0"],content_=str(b),page_=b["1"],domain_="")
       ddr.put()
       #Access-Control-Allow-Origin:*
-      self.response.write(ddr.key)
+      self.response.write(ddr.key())
 
 class AdminHandler(webapp2.RequestHandler):
   def get(self):
@@ -45,6 +46,12 @@ class AdminHandler(webapp2.RequestHandler):
       greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
                         (user.nickname(), users.create_logout_url('/')))
       self.response.write(greeting)
+      q = db.GqlQuery("select * from jserror order by date DESC ")
+      #q.order("-date")
+      results=q.fetch(limit=100)
+      self.response.write("<div style='padding-bottom:10px;'> --- </div>")
+      for jserror in results:
+        self.response.write("<div>%s - %s</div>" % (jserror.err_,jserror.date))
     else:
       self.redirect(users.create_login_url(self.request.uri))
 
